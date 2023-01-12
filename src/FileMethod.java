308f3113c2736c39a81b463cbdf7acf4211af8df
Arina <arina.milyutina.0005@gmail.com>
2023-01-12 14:15:44 +0300

    commit1

---
+++ src/FileMethod.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileMethod {
+    public void checkFile() {
+        File file = new File("history.txt");
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+    private final Writer writer = new ConsoleWriter();
+
+    public void readerFromFile() {
+
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader("history.txt"));
+            String s;
+            while ((s = bufferedReader.readLine()) != null) {
+                writer.write(s);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    }
+
